1b0efb0c0299f0f9b225d82adb7c64ad4266a2c6
Anusha98s <92158408+Anusha98s@users.noreply.github.com>
2021-10-20 12:57:49 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+
+namespace Demo_Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo Reflection in c#...........");
+
+            Type T = typeof(int);
+            Console.WriteLine("Type of Name : \t{0}", T.Name);
+            Console.WriteLine("Full name of the type defined:\t{0}", T.FullName);
+            Console.WriteLine("Type of Assembly in which it is defined :\t{0}", T.Assembly);
+            Console.WriteLine("Information about base Type :\t{0}", T.BaseType);
+
+            Dynamic_Object MyObj1 = new Dynamic_Object();
+            MyObj1.First_Method();
+            MyObj1.Second_Method();
+
+            dynamic MyObj2 = new Dynamic_Object();
+            //MyObj2.First_Method("Msg from Dynamic Object");
+            MyObj2.Second_Method();
+            MyObj2.First_Method();
+
+
+
+        }
+    }
+}
